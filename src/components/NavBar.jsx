fbe28f597c8030bb66e222dbe1029a7eeb1e097d
Stephen Whittaker <stephenwhittaker23@gmail.com>
2020-01-27 13:13:46 +0000

    attempt conditional rendering

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,18 +20,22 @@ class NavBar extends React.Component {
           <Link className="item" to="/add-property">
             Add a property
           </Link>
-          <FacebookLogin
-            appId="2526719230789556"
-            fields="name,email,picture"
-            callback={this.props.onLogin}
-          />
+          {!this.props.state.userID && (
+            <FacebookLogin
+              appId="2526719230789556"
+              fields="name,email,picture"
+              callback={this.props.onLogin}
+            />
+          )}
 
-          <div>
-            <Link className="item" callback={this.props.onLogout}>
-              <img src={this.props.state.picture} alt={this.props.state.name} />
-              <span>{this.props.state.name}</span>Sign out
-            </Link>
-          </div>
+          {this.props.state.userID && (
+            <div>
+              <Link className="item" callback={this.props.onLogout}>
+                <img src={this.props.state.picture} alt={this.props.state.name} />
+                <span>{this.props.state.name}</span>Sign out
+              </Link>
+            </div>
+          )}
         </ul>
       </div>
     );
